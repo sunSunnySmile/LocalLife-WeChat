1f531667914374d16f6df8b226dba6456c7cad6a
zouting <972721300@qq.com>
2017-12-31 16:35:41 +0800

    测试

--- pages/list/list.js
+++ pages/list/list.js
@@ -16,7 +16,7 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function(options) {
-        // console.log(options.cat); //获取的只是一个id
+        console.log(options.cat); //获取的只是一个id
         fetch(`categories/${options.cat}`)
             .then(
                 res => {
