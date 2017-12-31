ee494acd4f4f84ca5476077aaf7cbd8e30fbd5d5
zouting <972721300@qq.com>
2017-12-31 16:26:00 +0800

    本地生活完成

--- pages/list/list.js
+++ pages/list/list.js
@@ -6,7 +6,10 @@ Page({
      */
     data: {
         category: {},
-        shops: []
+        shops: [],
+        pageIndex: 0,
+        pageSize: 10,
+        hasMore: true
     },
 
     /**
@@ -23,13 +26,17 @@ Page({
                     wx.setNavigationBarTitle({
                         'title': this.data.category.name
                     })
-                    return fetch(`categories/${options.cat}/shops`, { _page: 1, _limit: 10 })
-                })
-            .then(
-                res => {
-                    this.setData({
-                        shops: res.data
-                    })
+
+                    // 调用测试loadMore
+                    this.loadMore()
+
+                    //         return fetch(`categories/${options.cat}/shops`, { _page: 1, _limit: 10 })
+                    //     })
+                    // .then(
+                    //     res => {
+                    //         this.setData({
+                    //             shops: res.data
+                    //         })
                 }
             )
     },
@@ -70,13 +77,20 @@ Page({
      * 页面相关事件处理函数--监听用户下拉动作
      */
     onPullDownRefresh: function() {
-
+        this.setData({
+            shops: [],
+            pageIndex: 0,
+            hasMore: true
+        })
+        this.loadMore()
     },
 
     /**
      * 页面上拉触底事件的处理函数
      */
     onReachBottom: function() {
+        // console.log('已经没有啦');
+        this.loadMore();
 
     },
 
@@ -85,5 +99,30 @@ Page({
      */
     onShareAppMessage: function() {
 
+    },
+    /**
+     * 封装一个loadMore函数，下拉加载更多数据
+     */
+    loadMore() {
+        if (!this.data.hasMore) return;
+        let { pageIndex, pageSize } = this.data;
+        const params = {
+            _page: ++pageIndex,
+            _limit: pageSize
+        }
+        fetch(`categories/${this.data.category.id}/shops`, params)
+            .then(
+                res => {
+                    console.log(res);
+                    const totalCount = parseInt(res.header['X-Total-Count']);
+                    const hasMore = pageIndex * pageSize < totalCount;
+                    const shops = this.data.shops.concat(res.data);
+                    this.setData({
+                        shops: shops,
+                        pageIndex: pageIndex,
+                        hasMore: hasMore
+                    })
+                }
+            )
     }
 })
